e5b43687c803e1ff6470844989cc97fa166ad8a2
guohanbj <guohanbj@yuanfudao.com>
2023-01-02 21:56:37 +0800

    chore: 修改部分目录结构

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  pageExtensions: [ "tsx" ],
   reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+    legacyBrowsers: false,
+    browsersListForSwc: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   compiler: {
     styledComponents: true,
   },
